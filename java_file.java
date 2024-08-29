e1fd7da641681df5fa08177176054fed571fa5d6
hamsa-2200031861 <dandalingareddy028@gmail.com>
2024-08-29 21:08:32 +0530

    Update java_file.java

---
+++ java_file.java
@@ -0,0 +1,7 @@
+public class java_class{
+  public static void main(String[] args){
+    System.out.println("Hello ,World");
+    System.out.println("Continues Integration");
+    System.out.println("Mavan Insatllation Process");
+  }
+}
